1187465532531e93a5f0a462600561f8c48c9661
shhr1214 <souhei0079@gmail.com>
2020-01-24 01:30:36 +0900

    path かえた

--- tools/env.zsh
+++ tools/env.zsh
@@ -3,4 +3,5 @@ alias ll="ls -l"
 
 alias ssh="TERM=xterm-256color ssh"
 
+export PATH="/usr/local/bin:${PATH}"
 export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:/usr/local/opt/coreutils/libexec/gnubin:${PATH}"
