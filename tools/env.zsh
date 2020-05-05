cb96ee15a2e49fae9734fb179a689876b7e43e4b
shhr1214 <souhei0079@gmail.com>
2020-05-06 07:45:53 +0900

    iroiro

--- tools/env.zsh
+++ tools/env.zsh
@@ -8,3 +8,8 @@ alias ssh="TERM=xterm-256color ssh"
 
 export PATH="/usr/local/bin:${PATH}"
 export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:/usr/local/opt/coreutils/libexec/gnubin:${PATH}"
+
+alias openblog="open https://shhr1214.gitlab.io/"
+alias openblog-dev="open http://localhost:1313/"
+alias blogrepo="cd ${HOME}/src/gitlab.com/shhr1214/shhr1214.gitlab.io"
+
