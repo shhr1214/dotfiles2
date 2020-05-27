88170c736036ac0f5aa5169f3a5c44fb6ac9ac23
shhr1214 <souhei0079@gmail.com>
2020-05-28 01:36:36 +0900

    home directory キレイにしたい

--- editor/env.zsh
+++ editor/env.zsh
@@ -5,7 +5,7 @@ alias ew="emacs-with-window"
 alias vi="nvim -u NONE --noplugin"
 alias vim="nvim"
 
-alias spacemacs="rm ${HOME}/.emacs.d && ln -fs ${HOME}/.spacemacs.emacs.d ${HOME}/.emacs.d && emacs"
-alias doomemacs="rm ${HOME}/.emacs.d && ln -fs ${HOME}/.doom.emacs.d ${HOME}/.emacs.d && emacs"
+alias spacemacs="rm ${HOME}/.emacs.d && ln -fs ${HOME}/.config/spacemacs.emacs.d ${HOME}/.emacs.d && emacs"
+alias doomemacs="rm ${HOME}/.emacs.d && ln -fs ${HOME}/.config/doom.emacs.d ${HOME}/.emacs.d && emacs"
 
-export PATH="${HOME}/.doom.emacs.d/bin:${PATH}"
+export PATH="${HOME}/.config/doom.emacs.d/bin:${PATH}"
