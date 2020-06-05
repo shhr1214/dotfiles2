131438f5a4c9ac71f0420e5d2f39bb6eea213809
shhr1214 <souhei0079@gmail.com>
2020-06-06 03:49:20 +0900

    python の user ディレクトリ使ってなかったんやん

--- lang/env.zsh
+++ lang/env.zsh
@@ -39,7 +39,7 @@ export NODE_PATH="${HOME}/.config/yarn/global/node_modules"
 export ELM_HOME="${HOME}/node_modules/elm"
 
 # python
-export PATH="/usr/local/opt/python/libexec/bin:${HOME}/Library/Python/3.7/bin:${PATH}"
+export PYTHONUSERBASE="${HOME}/.local"
 alias ipy=ipython
 
 # ruby
