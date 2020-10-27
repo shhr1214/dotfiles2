feb24f89c008d889d458592631b89b1ebc59e3de
shhr1214 <souhei0079@gmail.com>
2020-10-27 12:08:41 +0900

    emacs の XDG 対応

--- editor/env.zsh
+++ editor/env.zsh
@@ -5,7 +5,7 @@ alias ew="emacs-with-window"
 alias vi="nvim -u NONE --noplugin"
 alias vim="nvim"
 
-alias spacemacs="rm ${HOME}/.emacs.d && ln -fs ${HOME}/.config/spacemacs.emacs.d ${HOME}/.emacs.d && emacs"
-alias doomemacs="rm ${HOME}/.emacs.d && ln -fs ${HOME}/.config/doom.emacs.d ${HOME}/.emacs.d && emacs"
+alias spacemacs="rm ${HOME}/.config/emacs && ln -fs ${HOME}/.config/spacemacs.emacs.d ${HOME}/.config/emacs && emacs"
+alias doomemacs="rm ${HOME}/.config/emacs && ln -fs ${HOME}/.config/doom.emacs.d ${HOME}/.config/emacs && emacs"
 
 export PATH="${HOME}/.config/doom.emacs.d/bin:${PATH}"
